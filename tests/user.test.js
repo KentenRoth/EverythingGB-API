4f71b4f915c2abcc61ba8c8fa009895a796a08f0
Kentent Roth <kentencroth@yahoo.com>
2023-12-26 16:36:03 -0600

    Adding tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -30,10 +30,39 @@ describe('POST /users', () => {
 			.send(newUser)
 			.expect(400);
 	});
+
+	afterAll(async () => {
+		await User.deleteMany();
+	});
 });
 
 describe('GET /users', () => {
+	beforeAll(async () => {
+		const newUser = {
+			name: 'test',
+			email: 'test@test.com',
+			password: 'test1234',
+		};
+		await request(app).post('/users').send(newUser);
+	});
+
 	it('should get all users', async () => {
 		const response = await request(app).get('/users').expect(200);
+		expect(response.body.length).toEqual(1);
+	});
+
+	it('should have a name of test and email of test@test.com', async () => {
+		const response = await request(app).get('/users').expect(200);
+		expect(response.body[0].name).toEqual('test');
+		expect(response.body[0].email).toEqual('test@test.com');
+	});
+
+	it('should not have a password of test1234', async () => {
+		const response = await request(app).get('/users').expect(200);
+		expect(response.body[0].password).not.toEqual('test1234');
+	});
+
+	afterAll(async () => {
+		await User.deleteMany();
 	});
 });
