716dac47327e7a972704968aaf48bced0b309464
Kentent Roth <kentencroth@yahoo.com>
2023-12-31 16:29:25 -0600

    Setup recipe model

---
+++ server/Models/Recipe.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	ingredients: {
+		type: [String],
+		required: true,
+		trim: true,
+	},
+	ingredientsSetTwo: {
+		type: [String],
+		required: false,
+		trim: true,
+	},
+	instructions: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	notes: {
+		type: String,
+		required: false,
+		trim: true,
+	},
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+	},
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
